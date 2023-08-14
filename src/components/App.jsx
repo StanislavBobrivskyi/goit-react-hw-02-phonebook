912eafdf0284a7ad35bebea095555b8d3316e630
StanislavBobrivskyi <stanislavbobrivskyi@gmail.com>
2023-08-14 20:18:30 +0200

    task 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,75 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+    number: '',
+  };
+
+  handleAddContact = values => {
+    const { name, number } = values;
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+
+  render() {
+    const { contacts } = this.state;
+
+    return (
+      <div>
+        <h2>Phonebook</h2>
+        <Formik
+          initialValues={{ name: '', number: '' }}
+          onSubmit={(values, { resetForm }) => {
+            this.handleAddContact(values);
+            resetForm();
+          }}
+        >
+          <Form>
+            <div>
+              <p>Name</p>
+              <Field
+                type="text"
+                name="name"
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                required
+              />
+              <ErrorMessage name="name" component="div" />
+            </div>
+            <div>
+              <p>Phone number</p>
+              <Field
+                type="tel"
+                name="number"
+                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                required
+              />
+              <ErrorMessage name="number" component="div" />
+            </div>
+            <button type="submit">Add contact</button>
+          </Form>
+        </Formik>
+
+        <h2>Contacts</h2>
+        <ul>
+          {contacts.map(contact => (
+            <li key={contact.id}>
+              {contact.name}: {contact.number}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
